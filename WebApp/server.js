3f7edc14578705ae3f8913a68121db986a85ce95
rghossi <rafael.ghossi@gmail.com>
2016-07-11 23:04:38 -0500

    changing index file

--- WebApp/server.js
+++ WebApp/server.js
@@ -12,7 +12,7 @@ app.use(express.static(__dirname + '/'));
 app.get('/', function(req, res) {
 
     // make sure index is in the right directory. In this case /app/index.html
-    res.render('form');
+    res.render('index');
 });
 
 app.listen(port, function() {
