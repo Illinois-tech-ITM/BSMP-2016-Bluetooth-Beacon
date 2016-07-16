8bcdbb0a441a3384a55475e860b8375424403345
rghossi <rafael.ghossi@gmail.com>
2016-07-16 17:50:05 -0500

    fixed bug when beacon signal suddenly disappears

--- BluetoothBeacon/app/src/main/java/edu/iit/bluetoothbeacon/MainActivity.java
+++ BluetoothBeacon/app/src/main/java/edu/iit/bluetoothbeacon/MainActivity.java
@@ -9,6 +9,8 @@ import android.bluetooth.BluetoothDevice;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -25,7 +27,7 @@ import edu.iit.bluetoothbeacon.models.Translation;
 
 public class MainActivity extends AppCompatActivity implements OnResponseReceivedListener, MasterpieceFragment.OnLanguageSelectedListener {
     private final static int MIN_RSSI = -70;
-    private final static int NEARBY_RSSI = -55;
+    private final static int NEARBY_RSSI = -40;
     private static final int REQUEST_ENABLE_BT = 1;
 
     private Controller controller;
@@ -35,6 +37,13 @@ public class MainActivity extends AppCompatActivity implements OnResponseReceive
     private HashMap<BluetoothDevice, Integer> mDevicesList; //key: Beacon | value: RSSI (sinal strength)
     private String mCurrentLanguage;
 
+    private long mStart = 0;
+    private long mCurrentStart = 0;
+
+
+    private int mInterval = 5000; // 5 seconds by default, can be changed later
+    private Handler mHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,16 +51,26 @@ public class MainActivity extends AppCompatActivity implements OnResponseReceive
         mAdapter = BluetoothAdapter.getDefaultAdapter();
         mDevicesList = new HashMap<>();
         mCurrentLanguage = "pt-br";
+        mActiveDevice = null;
         controller = Controller.getInstance(this, this);
         switchToFragment(new WelcomeFragment().newInstance(), true);
+        mHandler = new Handler();
+        startRepeatingTask();
     }
 
     private LeScanCallback scanCallback = new LeScanCallback() {
         @Override
         public void onLeScan(BluetoothDevice bluetoothDevice, int rssi, byte[] bytes) {
+            long delta = (SystemClock.elapsedRealtime() - mStart);
+            if (mActiveDevice != null &&  delta > 2101546475){
+                Log.d("TEST", "Elapsed seconds: " + delta);
+                mActiveDevice = null;
+                switchToFragment(new WelcomeFragment().newInstance(), false);
+            }
             if (bluetoothDevice.getName() == null || !bluetoothDevice.getName().matches("DVC\\d\\d\\d\\d")) return;
 //            Log.d("Test", "Address: " + bluetoothDevice + " | RSSI: "+ rssi);
             mDevicesList.put(bluetoothDevice, rssi);
+
             if (mActiveDevice == null && rssi > NEARBY_RSSI){
                 mActiveDevice = bluetoothDevice;
                 controller.requestMasterpieceInfo(bluetoothDevice.getName().toLowerCase());
@@ -64,6 +83,10 @@ public class MainActivity extends AppCompatActivity implements OnResponseReceive
             } else if (mActiveDevice != null && bluetoothDevice.getAddress().equals(mActiveDevice.getAddress()) && rssi < MIN_RSSI){
                 mActiveDevice = null;
                 switchToFragment(new WelcomeFragment().newInstance(), false);
+            }
+            if (mActiveDevice != null && bluetoothDevice.getAddress().equals(mActiveDevice.getAddress())){
+                mStart = SystemClock.elapsedRealtime();
+                Log.d("Test", "Start time: " + mStart);
             }
         }
     };
@@ -95,6 +118,7 @@ public class MainActivity extends AppCompatActivity implements OnResponseReceive
     protected void onStop() {
         super.onStop();
         mAdapter.stopLeScan(scanCallback);
+        stopRepeatingTask();
     }
 
 //    @Override
@@ -145,5 +169,31 @@ public class MainActivity extends AppCompatActivity implements OnResponseReceive
     @Override
     public void onLanguageSelected(String language) {
         mCurrentLanguage = language;
+    }
+
+    Runnable mStatusChecker = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                if (mCurrentStart == mStart){
+                    mActiveDevice = null;
+                    switchToFragment(new WelcomeFragment().newInstance(), false);
+                } else {
+                    mCurrentStart = mStart;
+                }
+            } finally {
+                // 100% guarantee that this always happens, even if
+                // your update method throws an exception
+                mHandler.postDelayed(mStatusChecker, mInterval);
+            }
+        }
+    };
+
+    void startRepeatingTask() {
+        mStatusChecker.run();
+    }
+
+    void stopRepeatingTask() {
+        mHandler.removeCallbacks(mStatusChecker);
     }
 }
